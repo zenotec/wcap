be8983df306066cd02769e7053344547a7687beb
Kevin Mahoney <kevin.mahoney@zenotec.net>
2019-11-04 18:23:44 -0700

    Added option to autogen for installing required Ubuntu packages
    Signed-off-by: Kevin Mahoney <kevin.mahoney@zenotec.net>

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
-sudo apt-get update
-sudo apt-get install automake autoconf pkg-config libtool
-sudo apt-get install libnl-3-dev libnl-genl-3-dev libnl-route-3-dev
+if [ "xxx$1xxx" == "xxx-ixxx" ] || [ "xxx$1xxx" == "xxx--installxxx" ]; then
+  sudo apt-get update
+  sudo apt-get install automake autoconf pkg-config libtool
+  sudo apt-get install libnl-3-dev libnl-genl-3-dev libnl-route-3-dev
+fi
 
 mkdir -p ./build ./debug ./config ./m4
 touch NEWS README AUTHORS ChangeLog
